03aa152eb8740a85239384b9967aaed59016d011
LS <ls@MacBook-Air-de-Administrador-2.local>
2021-09-20 20:38:32 -0300

    refact code

--- aim766/ordena.c
+++ aim766/ordena.c
@@ -16,7 +16,6 @@ Nota: a avaliação será de 0-10. 0 para o trabalho não entregue e 10 para o t
 *******************************************************************************/
 
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>// necessário p/ as funções rand() e srand()
 #include<time.h>//necessário p/ função time()
 
@@ -29,75 +28,115 @@ int main()
     int max = 0;
     int min = 0;
     int c, k;
-    int aux;
-    
+    int aux, operationNum;
+    char fileName[50];
+    char fileNameInvert[50];
 
+    FILE *createFile;
+    srand(time(NULL));
+    
     printf("Quantos numeros deseja gerar na lista: ");
     scanf("%d", &numlist);  
     printf("numero = %d \n\n",numlist);
+    printf("Digite o nome do arquivo: ");
+    scanf("%s",fileName);
     
-    srand(time(NULL));
-    
-        if (numlist >= 1) {
-            printf(" Valor Original: ");
-             for(i = 0; i < numlist; i++) {
-                vetOriginal[i] = rand() % 100;
-                printf(" %d ", vetOriginal[i]);
-            }
-           
-            printf("\n Valor Invertido: ");
-            for(i = 0; i < numlist; i++) {
-                vetInvert[i] = vetOriginal[numlist-i-1];
-                printf(" %d ", vetInvert[i]);
-            }
-            /*Valor ordenado*/
-            for(i = 0; i < numlist; i++) {
-                for( j=i+1; j<numlist; j++ ){
-                    if( vetOriginal[i]  > vetOriginal[j]  ){
-                        aux = vetOriginal[i];
-                        vetOriginal[i] = vetOriginal[j];
-                        vetOriginal[j] = aux;
-                        }
+    createFile = fopen(fileName, "w");
+  
+    if (createFile==NULL && numlist >= 1) {
+      printf("Falha ao criar o arquivo.");
+      exit(1); 
+  } 
+    else {
+      printf(" Valor Original: ");
+        for(i = 0; i < numlist; i++) {
+          vetOriginal[i] = rand() % 100;
+          fprintf(createFile, "%d; ", vetOriginal[i]);
+          printf(" %d ", vetOriginal[i]);
+  }
+
+}
+do{
+    printf("\n\n===Escolha umas das opções abaixo===");
+    printf("\n1- Inverter Elementos.");
+    printf("\n2- Gerar novo arquivo com elementos ordenados.");
+    printf("\n3- Novo arquivo com maior e menor valor encontrado.");
+    printf("\n4- Novo arquivo com numeros convertidos para binario.");
+    printf("\n5- Sair.");
+    printf("\nDigite uma opcao:  ");
+    scanf("%d", &operationNum);
+ 
+    switch(operationNum){
+    case 1:
+      printf("Digite o nome do arquivo invertido: ");
+      scanf("%s",fileNameInvert);
+      createFile = fopen(fileNameInvert, "w");
+      for(i = 0; i < numlist; i++) {
+          vetInvert[i] = vetOriginal[numlist-i-1];
+
+          fprintf(createFile, "%d; ", vetInvert[i]);
+
+          printf(" %d ", vetInvert[i]);
+        }
+    break;
+    case 2: 
+      printf("Digite o nome do arquivo: ");
+      scanf("%s",fileName);
+        for(i = 0; i < numlist; i++) {
+          for( j=i+1; j<numlist; j++ ){
+            if( vetOriginal[i]  > vetOriginal[j]  ){
+                aux = vetOriginal[i];
+                vetOriginal[i] = vetOriginal[j];
+                vetOriginal[j] = aux;
                 }
+              }
             }
-            printf("\n Valor Ordenado:");
-            for(i=0;i<=numlist;i++)
-            {
-            printf(" %d ",vetOriginal[i]);
-            }     
-            /*Valor ordenado*/
-            printf("\n");
-            for(i = 0; i < numlist; i++) {
-                if (vetOriginal[i]>max)
+          printf("\n Valor Ordenado:");
+            for(i=0;i<=numlist;i++){
+              fprintf(createFile, "%d; ", vetOriginal[i]);
+              printf(" %d ",vetOriginal[i]);
+              }         
+    break;
+    case 3:
+        printf("\n");
+        printf("Digite o nome do arquivo: ");
+        scanf("%s",fileName);
+          for(i = 0; i < numlist; i++) {
+              if (vetOriginal[i]>max)
                     max = vetOriginal[i];
-                else 
+                else
                     if (vetOriginal[i]<min)
                        min = vetOriginal[i];
             }
-            
-            printf("\n\n Maior valor: %d\n", max);
-            /*printf("\n\n Menor valor %d\n", min);*/
-
-             
-
-              
+            fprintf(createFile, "%d; %d ", max,min);
+            printf("\n Maior valor: %d", max);
+            printf("\n Menor valor %d", min);
+      break;
+      case 4:
+        printf("Digite o nome do arquivo: ");
+        scanf("%s",fileName);
               for(i = 0; i < numlist; i++) {
-              printf("\n %d em em binario é: ", vetOriginal[i]);
+              printf("\n %d em binario: ", vetOriginal[i]);
               for (c = 40; c >= numlist; c--)
               {
                 k = vetOriginal[i] >> c;
-            
-                if (k & 1)
+                int o = 1;
+                int z = 0;
+
+                if (k & 1) {
+                  fprintf(createFile, "%d; ", o);
                   printf("1");
-                else
+                } else {
+                  fprintf(createFile, "%d; ", z);
                   printf("0");
+                }
               }
             }
-            
- 
-        } else {
-            printf("Erro: sem condição valida");
-        }
-
+      break;
+    default:
+      printf("opcao invalida! tente novamente...\n");
+      system ("pause");
+    }
+  } while(operationNum !=5);
 
 }
